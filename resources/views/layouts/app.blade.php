dcdc7ac1b85b97258e1c3941dd6f4ff9ff2af49f
rehamaratech <reham.b.123@gmail.com>
2020-04-22 12:04:00 -0700

    dashboard & statistics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,7 @@
         <script src="{{ asset('white') }}/js/white-dashboard.min.js?v=1.0.0"></script>
         <script src="{{ asset('white') }}/js/theme.js"></script>
 
+
         @stack('js')
 
         <script>
