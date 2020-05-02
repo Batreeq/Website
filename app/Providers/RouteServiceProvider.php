3b6db9a8bc4ac0884511ac2005169c4329b52aef
rehamaratech <reham.b.123@gmail.com>
2020-05-02 12:25:37 -0700

    updates 1

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const DASHBOARD = '/dashboard';
+    public const HOMEPAGE = '/home';
 
     /**
      * Define your route model bindings, pattern filters, etc.
